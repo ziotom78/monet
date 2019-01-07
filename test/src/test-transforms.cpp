94de1b7cf51cf525815b8729ebfb7018ee7e6654
Maurizio Tomasi <ziotom78@gmail.com>
2019-01-07 10:42:30 +0100

    Use CTest to run unit tests

---
+++ test/src/test-transforms.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <monet.hpp>
+
+using namespace monet;
+
+int main() {
+  TransformSequence seq =
+      rotate(Point(1.0, 2.0), 3.0) | translate(Point(4.0, 5.0));
+
+  assert(seq.size() == 2);
+
+  assert(seq.at(0).type == TransformType::Rotation);
+  assert(seq.at(0).rotation.pivot.x == 1.0);
+  assert(seq.at(0).rotation.pivot.y == 2.0);
+  assert(seq.at(0).rotation.angle == 3.0);
+
+  assert(seq.at(1).type == TransformType::Translation);
+  assert(seq.at(1).translation.x == 4.0);
+  assert(seq.at(1).translation.y == 5.0);
+
+  seq = seq | scalex(6.0);
+
+  assert(seq.size() == 3);
+  assert(seq.at(2).type == TransformType::Scale);
+  assert(seq.at(2).scale_factor.x == 6.0);
+  assert(seq.at(2).scale_factor.y == 1.0);
+}
